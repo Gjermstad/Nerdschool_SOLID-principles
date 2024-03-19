810e12e8296a17b6176e98a88ec5f83b9f2cfe56
Kenneth Gjermstad <keanbegj@gmail.com>
2024-03-19 19:38:41 +0100

    Initial commit

--- alarmsystem/src/main/java/nerdschool/SensorViewer.java
+++ alarmsystem/src/main/java/nerdschool/SensorViewer.java
@@ -2,16 +2,7 @@ package nerdschool;
 
 import java.util.List;
 
-public class SensorViewer
+public interface SensorViewer
 {
-  //# Methods
-  public void displayTriggeredSensors(List<Sensor> triggeredSensors) {
-    if (triggeredSensors.isEmpty()) {
-      System.out.println("No sensors were triggered");
-    } else {
-      for (Sensor sensor: triggeredSensors) {
-        System.out.printf("A %s sensor was triggered at %s%n", sensor.getSensorType(), sensor.getLocation());
-      }
-    }
-  }
+  void displayTriggeredSensors(List<Sensor> sensors);
 }
