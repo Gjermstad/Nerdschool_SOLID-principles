3ea1e56bf92d3929a67219c5d29199238afca321
Kenneth Gjermstad <keanbegj@gmail.com>
2024-03-19 19:38:43 +0100

    Initial commit

---
+++ alarmsystem/src/main/java/nerdschool/ConsoleSensorViewer.java
@@ -0,0 +1,17 @@
+package nerdschool;
+
+import java.util.List;
+
+public class ConsoleSensorViewer
+{
+  //# Methods
+  public void displayTriggeredSensors(List<Sensor> triggeredSensors) {
+    if (triggeredSensors.isEmpty()) {
+      System.out.println("No sensors were triggered");
+    } else {
+      for (Sensor sensor: triggeredSensors) {
+        System.out.printf("A %s sensor was triggered at %s%n", sensor.getSensorType(), sensor.getLocation());
+      }
+    }
+  }
+}
