03312313692440e8f7e170286d743e904cfe2991
Kenneth Gjermstad <keanbegj@gmail.com>
2024-03-19 19:31:01 +0100

    Removed Method for displaying triggered sensors to move into class SensorViewer

--- alarmsystem/src/main/java/nerdschool/ControlUnit.java
+++ alarmsystem/src/main/java/nerdschool/ControlUnit.java
@@ -29,13 +29,5 @@ public class ControlUnit {
         sensor.setBatteryPercentage();
       }
     }
-
-    if (triggeredSensors.isEmpty()) {
-      System.out.println("No sensors were triggered");
-    } else {
-      for (Sensor sensor: triggeredSensors) {
-        System.out.printf("A %s sensor was triggered at %s%n", sensor.getSensorType(), sensor.getLocation());
-      }
-    }
   }
 }
